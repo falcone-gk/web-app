00005e0da13701c3353ce93f05d9b8207595eaf8
Enzo Falcon <falcone134@gmail.com>
2022-07-20 13:22:59 -0500

    Creating project list view

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="projects">
+    <section class="projects">
+      <h1 class="section-title">Projects</h1>
+      <div class="project-list">
+        <CardProject v-for="(project, index) in projects" :key="'py-' + index"
+        :title="project['title']"
+        :description="project['description']"
+        :tags="project['tags']" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+import http from '@/helpers/http-common'
+import { ref } from 'vue';
+import CardProject from '@/components/CardProject.vue'
+
+const projects = ref([])
+
+const projectURL = 'api/projects'
+http.get(projectURL)
+  .then(response => {
+    projects.value = response.data.results
+  })
+</script>
+
+<style lang="scss" scoped>
+</style>
