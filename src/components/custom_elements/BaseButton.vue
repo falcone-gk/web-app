2dd38129c21509b3a65428bcc97d1fb70ea13b16
Enzo Falcon <falcone134@gmail.com>
2022-07-21 15:29:50 -0500

    Add a custom button for the app

---
+++ src/components/custom_elements/BaseButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button>{{ props.value }}</button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps(['value'])
+</script>
+
+<style lang="scss" scoped>
+button {
+  background-color: var(--primary-color);
+  border-radius: 5px;
+  border-style: none;
+  box-shadow: rgba(39, 174, 96, .15) 0 4px 9px;
+  color: #000;
+  cursor: pointer;
+  font-family: Inter,-apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1.5;
+  padding: 3px 20px;
+  transform: translate3d(0, 0, 0);
+  transition: all .3s;
+  white-space: nowrap;
+}
+
+button:hover {
+  background-color: var(--primary-dark-color);
+  box-shadow: rgba(39, 174, 96, .2) 0 6px 12px;
+  opacity: 1;
+  transform: translateY(0);
+  transition-duration: .35s;
+}
+
+</style>
