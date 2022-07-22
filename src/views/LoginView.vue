f78d3e191efe58e8aa16380404f4046c1917d856
Enzo Falcon <falcone134@gmail.com>
2022-07-21 20:59:13 -0500

    Adding login event

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,7 +11,7 @@
       label="Password"
       name="password"
       type="password" />
-      <BaseButton value="Login"/>
+      <BaseButton @click.prevent="onLogin" value="Login"/>
     </form>
   </div>
 </template>
@@ -20,11 +20,23 @@
 import { reactive } from 'vue';
 import InputForm from '@/components/custom_elements/InputForm.vue';
 import BaseButton from '@/components/custom_elements/BaseButton.vue';
+import http from '@/helpers/http-common';
 
 const data = reactive({
   'username': '',
   'password': ''
 })
+
+const onLogin = () => {
+  const loginUrlApi = '/token-auth/'
+  http.post(
+    loginUrlApi,
+    data
+  ).then(response => {
+    // Storing token in local storage.
+    localStorage.setItem('token', response.data.token)
+  }).catch(error => console.log(error.response.data))
+}
 </script>
 
 <style lang="scss" scoped>
