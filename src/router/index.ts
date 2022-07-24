817a00043d37532efc25194b74e595f92a944072
Enzo Falcon <falcone134@gmail.com>
2022-07-24 17:34:08 -0500

    Add authentication requirement in admin view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,24 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 import routes from '@/router/routes/index'
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes: routes
+})
+
+
+router.beforeEach((to, from, next) => {
+  const store = useAuthStore()
+  const authenticated: boolean = store.isAuthenticated
+  const onlyAdmin: boolean = to.matched.some(record => record.meta.isAdmin)
+  if (!authenticated && onlyAdmin) {
+    return next({
+      path: '/login',
+      query: { redirect: to.fullPath }
+    })
+  }
+  next()
 })
 
 export default router
