60819ecdb995123bed15f59ed3322dfc3c00b1ea
Enzo Falcon <falcone134@gmail.com>
2022-07-25 11:51:43 -0500

    Add admin tab for admin users

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,14 +7,17 @@
       <router-link to="/">Home</router-link>
       <router-link to="/projects">Projects</router-link>
       <router-link to="/blog">Blog</router-link>
+      <router-link v-if="authStore.isAuthenticated" to="/admin">Admin</router-link>
     </nav>
   </header>
 </template>
 
 <script setup lang="ts">
 import { useMq } from "vue3-mq"
+import { useAuthStore } from "@/stores/auth";
 
 const mq = useMq()
+const authStore = useAuthStore()
 </script>
 
 <style lang="scss" scoped>
