397af88c25049603269909f42ecd7111f6b20caf
Enzo Falcon <falcone134@gmail.com>
2022-07-25 17:08:13 -0500

    Refactor login button

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,7 +11,7 @@
       label="Password"
       name="password"
       type="password" />
-      <BaseButton @click.prevent="onLogin" value="Login"/>
+      <BaseButton @click.prevent="authStore.login(data)" value="Login"/>
     </form>
   </div>
 </template>
@@ -19,7 +19,7 @@
 <script setup lang="ts">
 import { reactive } from 'vue';
 import { useAuthStore } from '@/stores/auth'
-import { RouteLocationNormalizedLoaded, Router, useRoute, useRouter } from 'vue-router';
+import { Router, useRouter } from 'vue-router';
 import InputForm from '@/components/custom_elements/InputForm.vue';
 import BaseButton from '@/components/custom_elements/BaseButton.vue';
 
@@ -29,39 +29,18 @@ const data = reactive({
 })
 
 const router: Router = useRouter()
-const route: RouteLocationNormalizedLoaded = useRoute()
 const authStore = useAuthStore()
-
-// Check if there is a redirect in the url query. In case of existing then
-// user is redirected to that url if not then user goes to 'admin' page.
-const goToNext = () => {
-  const redirect: string | undefined = route.query.redirect as string | undefined
-  if (redirect) {
-    router.push({path: redirect})
-  } else {
-    router.push({path: '/admin'})
-  }
-}
 
 // Function that runs when component is created.
 const beforeCreate = () => {
   if (authStore.isAuthenticated) {
-    goToNext()
+    router.push('/admin')
   }
 }
 
 // running function to make some validations.
 beforeCreate()
 
-// Login event
-const onLogin = async () => {
-  await authStore.login(data)
-    .then(() => {
-      // In case that credentials sent are correct the user is redirect to admin page
-      // or redirect to previous page.
-      goToNext()
-    })
-}
 </script>
 
 <style lang="scss" scoped>
