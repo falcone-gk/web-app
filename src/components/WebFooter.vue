4477e74ddc81906d4f36b7d173b016826224e8e8
Enzo Falcon <falcone134@gmail.com>
2022-07-25 12:49:15 -0500

    Add login and logout in footer

--- src/components/WebFooter.vue
+++ src/components/WebFooter.vue
@@ -1,14 +1,29 @@
 <template>
   <footer>
-    <p>Are you and admin? then <a @click.prevent="toLogin">Login</a></p>
+    <p v-if="!authStore.isAuthenticated">Are you and admin? then <a @click.prevent="toLogin">Login</a></p>
+    <p v-else>Are you done? well, <a @click.prevent="toLogout">Logout</a></p>
   </footer>
 </template>
 
 <script setup lang="ts">
-import { Router, useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
+import { Router, useRouter, useRoute, RouteLocationNormalizedLoaded } from 'vue-router';
 
 const router: Router = useRouter()
-const toLogin = () => router.push({path: '/login'})
+const route: RouteLocationNormalizedLoaded = useRoute()
+const authStore = useAuthStore()
+
+// Send to login page with a redirect query.
+const toLogin = () => {
+  router.push({path: '/login', query: { redirect: route.fullPath }})
+}
+
+// Logout that reset authStore to default values (no user data at all)
+// and redirect to 'login' page.
+const toLogout = () => {
+  authStore.$reset()
+  router.push({ path: '/login' })
+}
 </script>
 
 <style lang="scss" scoped>
