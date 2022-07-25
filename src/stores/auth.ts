7f2ae91f4dff75874915480d7ef42de9d458f471
Enzo Falcon <falcone134@gmail.com>
2022-07-24 19:42:25 -0500

    Add actions to authStore

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,17 +1,41 @@
 import { defineStore } from 'pinia'
+import { useModalStore } from './modal';
+import http from '@/helpers/http-common';
 
 interface user {
   isAuthenticated: boolean,
-  token: string
-  username: string
+  token: string | null
+  username: string | null
 }
 
 export const useAuthStore = defineStore('auth', {
   state: (): user => {
     return {
       isAuthenticated: false,
-      token: '',
-      username: '',
+      token: null,
+      username: null,
+    }
+  },
+  actions: {
+    async login(data: {username: string, password: string}) {
+      const modalStore = useModalStore()
+
+      const url = '/token-auth/'
+      try {
+        // Making a post request to verify if user is an admin.
+        const tokenData = await http.post(url, data)
+        // Updating auth store data when user have correct credentials
+        this.isAuthenticated = true
+        this.token = tokenData.data.token
+        this.username = data.username
+      } catch(error) {
+        // In case user data is not correct we show modal with the error.
+        modalStore.$patch({
+          title: 'Authentication error',
+          message: 'Credentials sent to server are not correct.',
+          isActive: true
+        })
+      }
     }
   }
 })
