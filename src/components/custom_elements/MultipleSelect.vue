a0a017695feb92aee3025246d3bdb4e7e3524ad0
Enzo Falcon <falcone134@gmail.com>
2022-07-27 16:55:57 -0500

    Create multi select input form

---
+++ src/components/custom_elements/MultipleSelect.vue
@@ -0,0 +1,14 @@
+<template>
+  <label :for="props.name">{{ props.label }}</label>
+  <select :name="props.name" :id="props.name" multiple>
+    <option v-for="(opt, index) in props.options" :key="'opt-'+ index" :value="opt.id">{{ opt.tag }}</option>
+  </select>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['label', 'name', 'options'])
+</script>
+
+<style lang="scss" src="@/styles/components/_multi-select.scss" scoped />
