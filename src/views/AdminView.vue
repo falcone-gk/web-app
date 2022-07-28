13bdc980567f467a751750b9f6a2e2f0bddaf36a
Enzo Falcon <falcone134@gmail.com>
2022-07-28 18:01:44 -0500

    Get list tags and adding reactive data in projects

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -3,11 +3,11 @@
     <section class="admin-projects">
       <h1>Project form</h1>
       <form class="admin-projects">
-        <InputForm label="Title" name="title" placeholder="title" />
-        <TextareaField label="Description" name="description" />
-        <MultipleSelect label="Tags" name="tags" :options="options" />
-        <InputForm label="Source code" name="source-code" type="url" placeholder="Source code" />
-        <InputForm label="Source app" name="source-app" type="url" placeholder="Source app" />
+        <InputForm v-model="projectData.title" label="Title" name="title" placeholder="title" />
+        <TextareaField v-model="projectData.description" label="Description" name="description" />
+        <MultipleSelect v-model="projectData.tags" label="Tags" name="tags" :options="options" />
+        <InputForm v-model="projectData.source_code" label="Source code" name="source-code" type="url" placeholder="Source code" />
+        <InputForm v-model="projectData.source_app" label="Source app" name="source-app" type="url" placeholder="Source app" />
       </form>
     </section>
     <section class="admin-posts">
@@ -34,31 +34,43 @@
 import InputForm from '@/components/custom_elements/InputForm.vue';
 import TextareaField from '@/components/custom_elements/TextareaField.vue';
 import MultipleSelect from '@/components/custom_elements/MultipleSelect.vue';
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 import { computed } from '@vue/reactivity';
 import { marked } from 'marked';
+import http from '@/helpers/http-admin'
 
 interface tag {
   id: number,
-  tag: string
+  name: string
 }
 
-const options: tag[] = [
-  {
-    id: 1,
-    tag: 'Python',
-  },
-  {
-    id: 2,
-    tag: 'Vue'
-  }
-]
-
+const options = ref<tag | null>(null)
+const projectData = reactive({
+  title: '',
+  description: '',
+  tags: [],
+  source_code: '',
+  source_app: '',
+})
 const tab = ref("code")
 const markdown = ref("[comment]: <> (Write your post with markdown syntax!)")
+
 const htmlMarkdown = computed(() => {
   return marked(markdown.value)
 })
+
+// Get tags list.
+const getTags = async () => {
+  try {
+    const url = '/api/list-tags'
+    const response = await http.get(url)
+    options.value = response.data
+  } catch(error) {
+    return error
+  }
+}
+
+getTags()
 </script>
 
 
