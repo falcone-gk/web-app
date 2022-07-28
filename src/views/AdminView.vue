5bb20b7aa2efce173dded13e9d4fb131a343a4cb
Enzo Falcon <falcone134@gmail.com>
2022-07-27 19:52:15 -0500

    Add post form

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,36 +1,40 @@
 <template>
-  <section class="admin-projects">
-    <h1>Project form</h1>
-    <form class="admin-projects">
-      <InputForm
-      label="Title"
-      name="title"
-      placeholder="title" />
-      <TextareaField
-      label="Description"
-      name="description" />
-      <MultipleSelect
-      label="Tags"
-      name="tags"
-      :options="options" />
-      <InputForm
-      label="Source code"
-      name="source-code"
-      type="url"
-      placeholder="Source code" />
-      <InputForm
-      label="Source app"
-      name="source-app"
-      type="url"
-      placeholder="Source app" />
-    </form>
-  </section>
+  <div class="admin">
+    <section class="admin-projects">
+      <h1>Project form</h1>
+      <form class="admin-projects">
+        <InputForm label="Title" name="title" placeholder="title" />
+        <TextareaField label="Description" name="description" />
+        <MultipleSelect label="Tags" name="tags" :options="options" />
+        <InputForm label="Source code" name="source-code" type="url" placeholder="Source code" />
+        <InputForm label="Source app" name="source-app" type="url" placeholder="Source app" />
+      </form>
+    </section>
+    <section class="admin-posts">
+      <h1>Post form</h1>
+      <form>
+        <InputForm label="Title" name="title" placeholder="title" />
+        <TextareaField label="Description" name="description" />
+        <div class="editor">
+          <div class="tabs">
+            <input v-model="tab" value="code" type="radio" name="tabs" id="code" checked />
+            <label for="code">Raw Code</label>
+            <input v-model="tab" value="preview" type="radio" name="tabs" id="preview" />
+            <label for="preview">Preview</label>
+          </div>
+          <textarea v-if="tab === 'code'" name="post-markdown" id="post-input" rows="50"></textarea>
+          <div v-if="tab === 'preview'" class="preview-post"></div>
+        </div>
+      </form>
+    </section>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import InputForm from '@/components/custom_elements/InputForm.vue';
 import TextareaField from '@/components/custom_elements/TextareaField.vue';
 import MultipleSelect from '@/components/custom_elements/MultipleSelect.vue';
+import { ref } from 'vue';
 
 interface tag {
   id: number,
@@ -47,6 +51,9 @@ const options: tag[] = [
     tag: 'Vue'
   }
 ]
+
+const tab = ref("code")
 </script>
+
 
 <style lang="scss" src="@/styles/pages/_admin.scss" scoped />
