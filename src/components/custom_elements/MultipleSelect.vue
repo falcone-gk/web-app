682b6937f214592eba09a48ff88b1b07cfa02dd3
Enzo Falcon <falcone134@gmail.com>
2022-07-28 18:01:10 -0500

    Add emit to update multiple selected values

--- src/components/custom_elements/MultipleSelect.vue
+++ src/components/custom_elements/MultipleSelect.vue
@@ -1,14 +1,24 @@
 <template>
   <label :for="props.name">{{ props.label }}</label>
-  <select :name="props.name" :id="props.name" multiple>
-    <option v-for="(opt, index) in props.options" :key="'opt-'+ index" :value="opt.id">{{ opt.tag }}</option>
+  <select @input="handleInput" ref="refSelect" :name="props.name" :id="props.name" multiple>
+    <option v-for="(opt, index) in props.options" :key="'opt-'+ index" :value="opt.id">{{ opt.name }}</option>
   </select>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue';
+import { defineProps, defineEmits, ref } from 'vue';
 
-const props = defineProps(['label', 'name', 'options'])
+const props = defineProps(['modelValue', 'label', 'name', 'options'])
+const emits = defineEmits(['update:modelValue'])
+
+const refSelect = ref<HTMLSelectElement | null>(null)
+
+const handleInput = (): void => {
+  const selectedValues = [...Array.prototype.slice.call(refSelect.value?.options)]
+                        .filter(x => x.selected)
+                        .map(x => x.value);
+  emits('update:modelValue', selectedValues)
+}
 </script>
 
 <style lang="scss" src="@/styles/components/_multi-select.scss" scoped />
