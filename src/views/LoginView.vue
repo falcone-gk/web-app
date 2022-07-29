a755c96652f63d30e93b52d0901c0ab8253b20fc
Enzo Falcon <falcone134@gmail.com>
2022-07-28 20:01:49 -0500

    Add vuelidate in login form validation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,27 +5,39 @@
       v-model="data.username"
       label="Username"
       name="username"
-      type="text" />
+      :error="v.username.$errors[0]" />
       <InputForm
       v-model="data.password"
       label="Password"
       name="password"
-      type="password" />
-      <button @click.prevent="authStore.login(data)" class="app-btn">Login</button>
+      type="password"
+      :error="v.password.$errors[0]" />
+      <button @click.prevent="onLogin" class="app-btn">Login</button>
     </form>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, computed } from 'vue';
 import { useAuthStore } from '@/stores/auth'
 import { Router, useRouter } from 'vue-router';
+import useVuelidate from '@vuelidate/core';
+import { required } from '@vuelidate/validators';
 import InputForm from '@/components/custom_elements/InputForm.vue';
 
 const data = reactive({
   username: '',
   password: ''
 })
+
+const dataRules = computed(() => {
+  return {
+    username: { required },
+    password: { required }
+  }
+})
+
+const v = useVuelidate(dataRules, data)
 
 const router: Router = useRouter()
 const authStore = useAuthStore()
@@ -39,6 +51,13 @@ const beforeCreate = () => {
 
 // running function to make some validations.
 beforeCreate()
+
+const onLogin = async () => {
+  const isFormCorrect: boolean = await v.value.$validate()
+  if (!isFormCorrect) return
+
+  authStore.login(data)
+}
 
 </script>
 
