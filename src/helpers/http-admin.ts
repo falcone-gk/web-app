1f5ac86eeecfaca814ce8c507d59e3767230576e
Enzo Falcón <falcone134@gmail.com>
2022-07-31 19:07:56 -0500

    Fix getting token from pinia store

--- src/helpers/http-admin.ts
+++ src/helpers/http-admin.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosInstance } from "axios"
+import { useAuthStore } from '@/stores/auth'
 
-const token: string | null = localStorage.getItem('token')
+const authStore = useAuthStore()
+const token: string | null = authStore.token
 
 const http: AxiosInstance = axios.create({
   baseURL: 'http://127.0.0.1:8000/',
