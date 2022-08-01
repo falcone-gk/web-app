512dc7a42bcbcf8794c1324467120c635acc82f5
Enzo Falcón <falcone134@gmail.com>
2022-07-31 22:24:07 -0500

    Finish post form logic

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -14,8 +14,8 @@
     <section class="admin-posts">
       <h1>Post form</h1>
       <form>
-        <InputForm label="Title" name="title" placeholder="title" />
-        <TextareaField label="Description" name="description" />
+        <InputForm v-model="postData.title" :error="vPostData.title.$errors[0]" label="Title" name="title" placeholder="title" />
+        <TextareaField v-model="postData.description" :error="vPostData.description.$errors[0]" label="Description" name="description" />
         <div class="editor">
           <div class="tabs">
             <input v-model="tab" value="code" type="radio" name="tabs" id="code" checked />
@@ -24,8 +24,10 @@
             <label for="preview">Preview</label>
           </div>
           <textarea v-if="tab === 'code'" v-model="postData.body" name="post-markdown" id="post-input" rows="50" />
+          <span v-if="vPostData.body.$errors[0]" class="error-msg">{{ vPostData.body.$errors[0].$message }}</span>
           <div v-if="tab === 'preview'" v-html="htmlMarkdown" class="preview-post"></div>
         </div>
+        <button @click.prevent="submitPost" class="app-btn" type="submit">Create Post</button>
       </form>
     </section>
   </div>
@@ -74,18 +76,6 @@ const rulesProjectData = computed(() => {
 // Creates form validation for project form
 const vProjectData = useVuelidate(rulesProjectData, projectData)
 
-// Tab ref to know which tab is currently active.
-const tab = ref("code")
-const postData = reactive({
-  title: '',
-  description: '',
-  body: '[comment]: <> (Write your post with markdown syntax!)'
-})
-
-const htmlMarkdown = computed(() => {
-  return marked(postData.body)
-})
-
 // Logic to submit a new project.
 const submitProject = async () => {
   const isFormCorrect = await vProjectData.value.$validate()
@@ -97,6 +87,47 @@ const submitProject = async () => {
     modalStore.$patch({
       title: 'Successful request',
       message: 'New project was created successfuly!',
+      isActive: true
+    })
+  } catch(error) {
+    const err = error as AxiosError
+    return err
+  }
+}
+
+// Tab ref to know which tab is currently active.
+const tab = ref("code")
+// Reactive data to store post form values.
+const postData = reactive({
+  title: '',
+  description: '',
+  body: '[comment]: <> (Write your post with markdown syntax!)'
+})
+// Rules to validate post form before submit
+const rulesPostData = computed(() => {
+  return {
+    title: { required },
+    description: { required },
+    body: { required }
+  }
+})
+// Computed property to transform markdown to html
+const htmlMarkdown = computed(() => {
+  return marked(postData.body)
+})
+// Create form validation
+const vPostData = useVuelidate(rulesPostData, postData)
+// Post submit logic
+const submitPost = async () => {
+  const isFormCorrect = await vPostData.value.$validate()
+  if (!isFormCorrect) return
+
+  try {
+    const url = '/api/posts/'
+    await http.post(url, postData)
+    modalStore.$patch({
+      title: 'Successful request',
+      message: 'New post was created successfuly!',
       isActive: true
     })
   } catch(error) {
@@ -118,6 +149,5 @@ const getTags = async () => {
 
 getTags()
 </script>
-
 
 <style lang="scss" src="@/styles/pages/_admin.scss" scoped />
